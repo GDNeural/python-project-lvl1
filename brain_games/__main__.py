c15dfb05a9ad6f0992f87a33fab5adb768c83a58
GDNeural <hiperion.calm@gmail.com>
2020-05-21 13:25:56 +0300

    poetry_use

--- brain_games/__main__.py
+++
@@ -1,4 +0,0 @@
-from brain_games.scripts.brain_storm import mass_do
-
-if __name__ == "__main__":
-    mass_do()
--- brain_games/scripts/brain_storm.py
+++ brain_games/scripts/brain_storm.py
@@ -1,41 +1,72 @@
 import prompt
 import random
+import sys
 from brain_games.scripts.brain_calc import game_launch_brain_calc
 from brain_games.scripts.brain_even import game_launch_brain_even
 
 # Список игр, правил к ним и исполняемой функции
 
 game_list = [
-    ("Brain_games",'',),
-    ("Brain_calc", "What is the result of the expression?", game_launch_brain_calc),
-    ("Brain_even", "Answer \"yes\" if number even otherwise answer \"no\".", game_launch_brain_even)
+    ("brain-games",'', None),
+    ("brain-calc", "What is the result of the expression?\n", game_launch_brain_calc),
+    ("brain-even", "Answer \"yes\" if number even otherwise answer \"no\".\n", game_launch_brain_even)
 ]
 
-def mass_do():
 
+# 2 Способа запуска пакета
+
+
+# №1 Как исполняемый скрипт
+
+def module_run():
+    
+    # 1. Вывести список игр
+    print("Game list:")
+    for i in range(len(game_list)):
+        print(i+1,game_list[i][0], "\n")
+    
+    # 2. Узнать у пользователя какая игра ему нужна
+    users_input = prompt.integer("What do you want to play: ")
+    game = game_list[users_input-1]
+    
+    # 3. Напечатать правила
+    print(game[1])
+
+    #4. Запуск основной игры
+    launch_game(game)
+
+
+# №2 Через poetry
+
+def poetry_run():
+    users_comand = sys.argv[0]
+    game = 0
+    for g in game_list:
+        if users_comand == g[0]:
+            print(g[1])
+            game = g
+    
+    launch_game(game)
+
+# Общая логика для запуска всех игр
+    
+def launch_game(game):
     print ("Welcome to the Brain Games!\n")
 
     name = prompt.string('May I have your name? ')
     
     print('\nHello, %s!\n'%(name))
 
-    # 1. Вывести список игр
-    print("Game list:")
-    for i in range(len(game_list)):
-        print(i+1,game_list[i][0], "\n")
-    # 2. Узнать у пользователя какая игра ему нужна
-    users_input = prompt.integer("What do you want to play: ")
-    right_index = users_input - 1
-    # 3. Напечатать правила
-    print(game_list[right_index][1])
     # 4. Запустить нужную игру
     counter = 0
     while counter !=3:
-        condition = game_list[right_index][2]()
+        if game[2] == None:
+            break
+        condition = game[2]()
         if condition:
             print("Correct!\n")
             counter +=1
         else:
-            print("\nStop right there you criminal scum")
+            print("\nStop right there you criminal scum\n")
 
-    print("Congrats, %s!\n"%(name))     
+    print("Congrats, %s!\n"%(name)) 
